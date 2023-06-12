39017af2ecff0197e461a9b75b2b5f010b83bb6f
Rina Inada <indrn@icloud.com>
2023-06-12 16:51:50 +0000

    change to callback waterfall

--- survey.js
+++ survey.js
@@ -1,82 +1,27 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-const question1 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question(`What's your name? Nicknames are also acceptable :)`, (answer) => {
-      console.log(`${answer}`)
-      resolve()
-    })
-  })
-}
-
-const question2 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question(`What's an activity you like doing?`, (answer) => {
-      console.log(`${answer}`)
-      resolve()
-    })
-  })
-}
-
-const question3 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question(`What do you listen to while doing that?`, (answer) => {
-      console.log(`${answer}`)
-      resolve()
-    })
-  })
-}
-
-const question4 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question(`Which meal is your favourite (eg: dinner, brunch, etc.)`, (answer) => {
-      console.log(`${answer}`)
-      resolve()
-    })
-  })
-}
-
-const question5 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question(` What's your favourite thing to eat for that meal?`, (answer) => {
-      console.log(`${answer}`)
-      resolve()
-    })
-  })
-}
-
-const question6 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question(`Which sport is your absolute favourite?`, (answer) => {
-      console.log(`${answer}`)
-      resolve()
-    })
-  })
-}
-
-const question7 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question(`What is your superpower? In a few words, tell us what you are amazing at!`, (answer) => {
-      console.log(`${answer}`)
-      resolve()
-    })
-  })
-}
-
-const main = async () => {
-  await question1();
-  await question2();
-  await question3();
-  await question4();
-  await question5();
-  await question6();
-  await question7();
-  rl.close();
-}
-
-main();
+rl.question(
+  "What is your name? Nicknames are also accepyable : ",
+  (answer1) => {
+    rl.question("What is an activity you like doing? : ", (answer2) => {
+      rl.question("What do you listen to while doing that? : ", (answer3) => {
+        rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) : ", (answer4) => {
+          rl.question("What is your favourite thing to eat for that meal? : ", (answer5) => {
+            rl.question("Which sport is your absolute favourite? : ", (answer6) => {
+              rl.question("What is your superpower? In a few words, tell us what you are amazing at! : ", (answer7) => {
+                console.log(`My name is ${answer1}. An activity I like doing is ${answer2}. I listen to ${answer3} while doing it.`);
+                console.log(`My favourite meal is ${answer4}, and My favourite thing to eat for the meal is ${answer5}.`);
+                console.log(`My absolute favourite sport is ${answer6}, and my superpwer is that : ${answer7}`);
+                rl.close();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
